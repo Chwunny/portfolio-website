f30dbe9443546769c9830716b90d5098d4fcf498
Zach King <zacheking10@gmail.com>
2021-11-15 19:48:55 -0700

    version 1 of website complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', './app.reset.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'apm-new';
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
 }
